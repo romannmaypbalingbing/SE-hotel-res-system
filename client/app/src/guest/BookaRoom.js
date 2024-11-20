f24d17e28bb1db6e4bce177ff2ca4e843127b4a4
rom-ann <146835440+romannmaypbalingbing@users.noreply.github.com>
2024-11-20 17:59:49 +0800

    4th commit - changes to BookaRoom.js

--- client/app/src/guest/BookaRoom.js
+++ client/app/src/guest/BookaRoom.js
@@ -2,8 +2,24 @@ import React from 'react';
 import GuestNavBar from '../components/GuestNavBar';
 import Stepper from '../components/Stepper';
 
-export default class ReservationInfo extends React.Component {
-    render() {
+const BookaRoom = () => {
+    const [lastName, setLastName] = React.useState({
+        "id-01": "",
+    })
+    const [firstName, setFirstName] = React.useState({
+        "id-02": "",
+    })
+
+
+    const handleChange = evt => {
+        const value = evt.target.value;
+        setLastName({
+            ...lastName,
+            ...firstName,
+            [evt.target.name]: value
+        })
+    }
+
         return (
             <div className="bg-slate-100 h-screen"> {/* Set background to slate-100 */}
                 <GuestNavBar /> 
@@ -13,49 +29,80 @@ export default class ReservationInfo extends React.Component {
                 <div className="flex justify-center items-start h-screen pt-20">
                     <div className="bg-white p-6 w-1/2 shadow-md rounded-lg"> {/* Form container set to white with rounded corners */}
                         <div className="grid grid-cols-2 gap-4">
-                            <div className="flex flex-col"> {/* Added container for label and input */}
-                                <label htmlFor="check-in" className="block text-sm font-medium text-gray-700 text-left">
-                                    Check-In
-                                </label>
+                            {/*    <!-- Component: Rounded basic input  --> */}
+                            <div className="relative my-6">
                                 <input
-                                    type="date"
-                                    id="check-in"
-                                    className="mt-1 block w-full rounded-md border border-gray-200 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm py-2 px-3"
+                                id="id-01"
+                                type="text"
+                                name="id-01"
+                                placeholder="Last Name"
+                                className="peer relative h-10 w-full rounded border border-slate-400 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-red-900 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+                                onChange={handleChange}
                                 />
-                            </div>
-                            <div className="flex flex-col"> {/* Added container for label and input */}
-                                <label htmlFor="check-out" className="block text-sm font-medium text-gray-700 text-left">
-                                    Check-Out
+                                <label
+                                htmlFor="id-01"
+                                className="absolute left-2 -top-2 z-[1] cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-autofill:-top-2 peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-red-900 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+                                >
+                                Last Name
                                 </label>
-                                <input
-                                    type="date"
-                                    id="check-out"
-                                    className="mt-1 block w-full rounded-md border border-gray-200 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm py-2"
-                                />
                             </div>
-                            <div className="flex flex-col"> {/* Added container for label and input */}
-                                <label htmlFor="pax" className="block text-sm font-medium text-gray-700 text-left">
-                                    Number of Guests
+                            {/*    <!-- End Rounded basic input  --> */}
+
+                            {/*    <!-- Component: Rounded basic input  --> */}
+                            <div className="relative my-6">
+                                <input
+                                id="id-02"
+                                type="text"
+                                name="id-02"
+                                placeholder="First Name"
+                                className="peer relative h-10 w-full rounded border border-slate-400 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-red-900 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+                                onChange={handleChange}
+                                />
+                                <label
+                                htmlFor="id-01"
+                                className="absolute left-2 -top-2 z-[1] cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-autofill:-top-2 peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-red-900 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+                                >
+                                First Name
                                 </label>
-                                <input
-                                    type="number"
-                                    id="pax"
-                                    className="mt-1 block w-full rounded-md border border-gray-200 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm py-2 px-3"
-                                />
                             </div>
-                            <div className="flex flex-col">
-                            <label htmlFor="room-type" className="block text-sm font-medium text-gray-700 text-left">
-                                    Room Type
+                            {/*    <!-- End Rounded basic input  --> */}
+
+                            <div className="relative my-6">
+                                <input
+                                id="id-02"
+                                type="text"
+                                name="id-02"
+                                placeholder="First Name"
+                                className="peer relative h-10 w-full rounded border border-slate-400 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-red-900 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+                                onChange={handleChange}
+                                />
+                                <label
+                                htmlFor="id-01"
+                                className="absolute left-2 -top-2 z-[1] cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-autofill:-top-2 peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-red-900 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+                                >
+                                Email
                                 </label>
-                                <input
-                                    type="text"
-                                    id="room-type"
-                                    className="mt-1 block w-full rounded-md border border-gray-200 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm py-2 px-3"
-                                    placeholder="e.g. Deluxe, Suite"
-                                />
                             </div>
-                            <div /> {/* Empty div to maintain grid alignment */}
+
+                            <div className="relative my-6">
+                                <input
+                                id="id-02"
+                                type="text"
+                                name="id-02"
+                                placeholder="Contact Number"
+                                className="peer relative h-10 w-full rounded border border-slate-400 px-4 text-sm text-slate-500 placeholder-transparent outline-none transition-all autofill:bg-white invalid:border-pink-500 invalid:text-pink-500 focus:border-red-900 focus:outline-none invalid:focus:border-pink-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:bg-slate-50 disabled:text-slate-400"
+                                onChange={handleChange}
+                                />
+                                <label
+                                htmlFor="id-01"
+                                className="absolute left-2 -top-2 z-[1] cursor-text px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-white before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-autofill:-top-2 peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:cursor-default peer-focus:text-xs peer-focus:text-red-900 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
+                                >
+                                Contact Number
+                                </label>
+                                <div className="w-full border-t border-slate-300 mt-2"></div> {/* Adds line below the Contact Number field */}
+                            </div>
                         </div>
+                        
                         <div className="mt-6">
                             <button className="w-full bg-red-700 hover:bg-red-800 text-white font-bold py-2 px-4 rounded">
                                 Search
@@ -66,4 +113,4 @@ export default class ReservationInfo extends React.Component {
             </div>
         );
     }
-}
+export default BookaRoom;
